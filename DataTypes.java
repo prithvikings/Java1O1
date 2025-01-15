7e7d1a40a656b6fe7df1a7536b8d5d4b5fa5ac6e
prithvikings <prithvi07raj07@gmail.com>
2025-01-15 22:55:33 +0530

    completed with datatypes

---
+++ DataTypes.java
@@ -0,0 +1,50 @@
+/*
+Java is type safe language which means that all variables must first be declared before they can be used.
+
+Purpose: This file contains the data types used in the program.
+
+Data types include:
+In java there are 2 types of data types:
+1. Primitive data types are predefined by the language and named by a keyword.
+2. Non-primitive data types are created by the programmer and is not defined by the language.
+
+Primitive data types include:
+1. byte
+2. short
+3. int
+4. long
+5. float
+6. double
+7. char
+8. boolean
+
+Non-primitive data types include:
+1. String
+2. Array
+3. Class
+4. Interface
+5. etc.
+The data types are used to store different types of data in the program.
+*/
+public class DataTypes {
+    public static void main(String[] args){
+            byte num1 = 5;
+            System.out.println(num1);
+            short num2 = 4;
+            System.out.println(num2);
+            char ch='A';
+            System.out.println(ch);
+            int num3 = 10;
+            System.out.println(num3);
+            long num4 = 100;
+            System.out.println(num4);
+            float num5 = 5.5f;
+            System.out.println(num5);
+            double num6 = 10.5;
+            System.out.println(num6);
+            boolean b = true;
+            System.out.println(b);
+            String Player = "Ms Dhoni";
+            System.out.println(Player);
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,16 @@
+public class Variable{
+    public static void main(String[]args){
+        int num1 = 5;
+        System.out.println(num1);
+        int num2 = 4;
+        System.out.println(num2);
+        System.out.println(num1 + num2);
+        String Player = "Ms Dhoni";
+        System.out.println(Player);
+
+        num1= 10;
+        System.out.println(num1);
+        num2=num1;
+        System.out.println(num2);
+    }
+}
