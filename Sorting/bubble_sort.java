68c7107f3d69f6f0487d04f5e42f86f73cf20fe1
prithvikings <prithvi07raj07@gmail.com>
2025-01-19 00:50:16 +0530

    Refactor stock method to be commented out and add bubble sort and selection sort implementations

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,80 @@
+public class bubble_sort {
+
+    public static void printarr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSortAsc(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+
+
+    public static void bubbleSortDesc(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+
+    public static void selectionSortAsc(int arr[]){
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                int temp=arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=temp;
+            }
+        }
+    }
+
+    public static void selectionSortDesc(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            int minIndex=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]>arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                int temp=arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=temp;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 1, 3, 2 };
+        // bubbleSortAsc(arr);
+        // bubbleSortDesc(arr);
+        // selectionSortAsc(arr);
+        selectionSortDesc(arr);
+        printarr(arr);
+    }
+}
